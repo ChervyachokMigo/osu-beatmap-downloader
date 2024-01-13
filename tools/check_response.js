6f0adbcb1968e6147fe2f98ec02fac6eb7a11927
tester_pivka17 <topsherman88@gmail.com>
2024-01-14 03:42:00 +0500

    f

--- tools/check_response.js
+++ tools/check_response.js
@@ -1,9 +1,46 @@
 const fs = require('fs');
-var { sleep, log } = require(`./tools.js`);
+var { sleep, log, formatTime } = require(`./tools.js`);
 var  downloadquota  = require("./downloadquota.js");
 var  download_path  = require("./download_path.js");
+const dashboard = require('dashboard_framework');
 
 const { copy_beatmaps } = require('./copy_beatmaps.js');
+
+const get_waiting_quota_time = (start_time, waiting_mins) => {
+    const waiting_ms = waiting_mins * 60 * 1000;
+    let current_time = new Date().getTime();
+    let diff_time = current_time - start_time;
+    let reverse_diff_time = waiting_ms - diff_time;
+    if (reverse_diff_time > waiting_ms) {
+        return formatTime(0);
+    } else {
+        return formatTime(reverse_diff_time);
+    }
+}
+
+let waiting_quota_interval = null;
+
+const dashboard_waiting_quota_start = async () => {
+    await dashboard.change_status({name: 'download_quota', status: 'quota'});
+    let start_waiting_time = new Date().getTime();
+    waiting_quota_interval = setInterval( async () => {
+        await dashboard.change_text_item({
+            name: 'download_quota', 
+            item_name: 'quota', 
+            text: `предел, ожидание ${get_waiting_quota_time(start_waiting_time, 30)}`
+        });
+    }, 1000);
+}
+
+const dashboard_waiting_quota_end = async () => {
+    await dashboard.change_status({name: 'download_quota', status: 'ready'});
+    await dashboard.change_text_item({
+        name: 'download_quota', 
+        item_name: 'quota', 
+        text: `предел`
+    });
+    clearInterval(waiting_quota_interval);
+}
 
 module.exports =  async function check_response (response, beatmapname) {
     return new Promise ( async ( res, rej) => {
@@ -18,14 +55,13 @@ module.exports =  async function check_response (response, beatmapname) {
             log(response);
             log(`waiting 30 minutes for retry.`);
             
+            await dashboard_waiting_quota_start();
             await copy_beatmaps();
-            
             await sleep(1800);
-            
-            res (true);
-        } else {
-            res (true);
+            await dashboard_waiting_quota_end();
         }
+
+        res (true);
 
         
     });
--- tools/tools.js
+++ tools/tools.js
@@ -1,28 +1,57 @@
 const fs = require('fs');
 const colors = require('colors');
 
+const formatAddZero = (t, symbols = 1) => {
+    var result = t.toString();
+    var numberLength = t.toString().length;
+    if ( result.length < symbols){
+        for (var i = 0; i < symbols-numberLength; i++){
+            result = `0${result}`;
+        }
+    }
+    return result;
+}
+
+const get_time_string = (date) => {
+    return `${formatAddZero(date.getHours(),2)}:${formatAddZero(date.getMinutes(),2)}:${formatAddZero(date.getSeconds(),2)}`;
+}
+
+const get_date_string = (date) => {
+    return `${date.getFullYear()}-${formatAddZero(date.getMonth()+1, 2)}-${formatAddZero(date.getDate(), 2)}`;
+}
+
+const get_past_day = (currentdate) => {
+	var onedayms = 86400000;
+    var pastDayDate = new Date(new Date(currentdate)-onedayms);
+    return get_date_string(pastDayDate);
+}
+
+const get_past_week = (currentdate) => {
+	var onedayms = 86400000;
+
+    var pastWeekDate = new Date(new Date(currentdate) - onedayms * 7);
+    return get_date_string(pastWeekDate);
+}
+
+const get_past_month = (currentdate) => {
+	var onedayms = 86400000;
+
+    var pastMonthDate = new Date(new Date(currentdate) - onedayms * 30);
+    return get_date_string(pastMonthDate);
+}
+
 module.exports = {
-    get_past_day: function (currentDateString){
-        return  get_past_day(currentDateString);
-    },
+    get_past_day,
 
-    get_past_week: function (currentDateString){
-        return get_past_week(currentDateString);
-    },
+    get_past_week,
 
-    get_past_month: function (currentDateString){
-        return get_past_month(currentDateString);
-    },
+    get_past_month,
 
-    get_date_string: function (date){
-        return get_date_string(date);
-    },
+    get_date_string,
 
-    get_time_string: function (date){
-        return get_time_string(date);
-    },
+    get_time_string,
 
-    escapeString: function (text){
+    escapeString: (text) => {
         return text?text.replace(/[&\/\\#+$~%'":*?<>{}|]/g, ''):'';
     },
 
@@ -33,11 +62,11 @@ module.exports = {
         });
     },
 
-    log: function (...string){
+    log: (...string) => {
         console.log( `[${get_time_string(new Date()).yellow}]`, string.join(' ') );
     },
 
-    checkDir: function(path){
+    checkDir: (path) => {
         if (!fs.existsSync(`${path}`)) { fs.mkdirSync(`${path}`, {recursive: true});}
     },
 
@@ -45,44 +74,14 @@ module.exports = {
         return (current / max * 100).toFixed(digits)
     },
 
-}
-
-function get_time_string(date){
-    return `${formatAddZero(date.getHours(),2)}:${formatAddZero(date.getMinutes(),2)}:${formatAddZero(date.getSeconds(),2)}`;
-}
-
-function get_date_string(date){
-    return `${date.getFullYear()}-${formatAddZero(date.getMonth()+1, 2)}-${formatAddZero(date.getDate(), 2)}`;
+    formatAddZero,
     
-}
-
-function get_past_day(currentdate){
-	var onedayms = 86400000;
-    var pastDayDate = new Date(new Date(currentdate)-onedayms);
-    return get_date_string(pastDayDate);
-}
-
-function get_past_week(currentdate){
-	var onedayms = 86400000;
-
-    var pastWeekDate = new Date(new Date(currentdate) - onedayms * 7);
-    return get_date_string(pastWeekDate);
-}
-
-function get_past_month(currentdate){
-	var onedayms = 86400000;
-
-    var pastMonthDate = new Date(new Date(currentdate) - onedayms * 30);
-    return get_date_string(pastMonthDate);
-}
-
-function formatAddZero(t, symbols = 1){
-    var numberZeroed = t.toString();
-    var numberLength = t.toString().length;
-    if ( numberZeroed.length < symbols){
-        for (var i = 0; i < symbols-numberLength; i++){
-            numberZeroed = `0${numberZeroed}`;
-        }
+    formatTime: (time_ms) => {
+        const time_sec = Math.trunc(time_ms / 1000);
+        const time_min = Math.trunc(time_sec / 60);
+        const sec = time_sec % 60;
+        const min = time_min % 60;
+        return `${formatAddZero(min, 2)}:${formatAddZero(sec, 2)}`
     }
-    return numberZeroed;
+
 }
