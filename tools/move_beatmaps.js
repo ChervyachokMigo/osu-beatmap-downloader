378488d958466c30d845edad45bdf1e41d5f466c
ChervyachokMigo <s4d.god@yandex.ru>
2025-02-22 05:51:26 +0500

    open beatmaps in osu laser

--- tools/move_beatmaps.js
+++ tools/move_beatmaps.js
@@ -3,8 +3,11 @@ const config = require('../config.js');
 const { readdirSync, copyFileSync, rmSync } = require('node:fs');
 const path = require('node:path');
 const { download_path } = require('./download_path.js');
+const { execFile } = require("child_process");
 
-const move_file_sync = ({ src, dest }) => {
+const osu_laser_exe_path = path.join(process.env.LOCALAPPDATA, 'osulazer', 'current', 'osu!.exe');
+
+const move_single_file = ({ src, dest }) => {
     try{
         if (config.is_detail_move_log){
             log('move from',src);
@@ -21,17 +24,46 @@ const move_file_sync = ({ src, dest }) => {
     }
 }
 
-module.exports = () => {
-	log('moving files to Songs..');
-
-	const files = readdirSync(download_path, { encoding: 'utf8' });
+const move_beatmaps_osu_stable = (files) => {
+	log('moving files to "Songs" in osu stable..');
 
 	for (let filename of files) {
-
-		move_file_sync({
+		move_single_file({
 			src: path.join(download_path, filename), 
 			dest: path.join(config.osuFolder, 'Songs', filename)
 		});
-
 	}
+}
+
+const move_beatmaps_osulaser = (files) => {
+	log('opening files in osu laser..');
+	const files_args = files.map( v => path.join(download_path, v));
+
+	execFile(osu_laser_exe_path, files_args, (error, stdout, stderr) => {
+		if (error) {
+			console.error(`Error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.error(`stderr: ${stderr}`);
+			return;
+		}
+		console.log(`Successfully opened ${beatmap_name} in osu!Lazer.`);
+		console.log(stdout);
+	});
+	
+}
+
+module.exports = () => {
+	const files = readdirSync(download_path, { encoding: 'utf8' });
+	if (files.length === 0) {
+        return;
+    }
+
+	if (config.is_use_laser) {
+		move_beatmaps_osulaser(files);
+	} else {
+		move_beatmaps_osu_stable(files);
+	}
+
 }
